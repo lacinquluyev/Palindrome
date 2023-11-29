88b9a0e176e6783b714917a03b228ffea0b0dee4
lacinquluyev <lachinquluyev@gmail.com>
2023-11-29 20:52:54 +0400

    first commit

---
+++ src/Palindrome.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+//Tersi duzune beraber olan sozler veya reqemler toplusu palindrome adlanir.
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String soz;
+        String tersSoz = "";
+
+        System.out.print(" Sozu daxil edin : ");
+
+        soz=scanner.nextLine();
+        int uzunluq =soz.length();
+
+        for (int i = uzunluq-1; i >=0 ; i--) {
+            tersSoz += soz.charAt(i);
+        } if (soz.equals(tersSoz)){
+            System.out.println(soz + " = Palindrome sozdur ");
+        } else {
+            System.out.println(soz + " = Palindrome deyildir");
+        }
+    }
+}
